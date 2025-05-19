7e0bd5bb359268234f343040828f035abc95c1a1
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-05-19 12:45:26 -0400

    restructuring

--- deepttc_preprocess_improve.py
+++ deepttc_preprocess_improve.py
@@ -46,160 +46,23 @@ def process_gene_expression(data, gene_expression_columns, dtype=None):
 
     return df_gene_expression
 
-def preprocess(args, rna_data, drug_data, response_data, response_metric='AUC'):
-    #args["vocab_dir"] = '.'  # os.path.join(IMPROVE_DATA_DIR, 'DeepTTC')
-    obj = DataEncoding(args, args["input_supp_data_dir"], args["canc_col_name"],
-                       args["sample_col_name"], args["y_col_name"], args["drug_col_name"])
-    drug_smiles = drug_data
-
-    drugid2smile = dict(
-        zip(drug_smiles[args["drug_col_name"]], drug_smiles['SMILES']))
-    smile_encode = pd.Series(drug_smiles['SMILES'].unique()).apply(
-        obj._drug2emb_encoder)
-    uniq_smile_dict = dict(
-        zip(drug_smiles['SMILES'].unique(), smile_encode))
-
-    # drug_data.drop(['SMILES'], inplace=True, axis=1)
-    drug_data['smiles'] = [drugid2smile[i]
-                           for i in drug_data[args["drug_col_name"]]]
-    drug_data['drug_encoding'] = [uniq_smile_dict[i]
-                                  for i in drug_data['smiles']]
-    drug_data = drug_data.reset_index()
-
-    response_data = response_data[[
-        args["canc_col_name"], args["drug_col_name"], response_metric]]
-    response_data.columns = [args["canc_col_name"],
-                             args["drug_col_name"], 'Label']
-    drug_data = pd.merge(response_data, drug_data,
-                         on=args["drug_col_name"], how='inner')
-    # drug_data['Label'] = response_data['AUC']
-
-    # response_data = response_data[['CancID', 'DrugID', response_metric]]
-    # response_data.columns = ['CancID', 'DrugID', 'Label']
-    # response_data = response_data[['CancID', 'DrugID']]
-
-    # rna_data = pd.merge(response_data, rna_data, on='CancID', how='inner')
-    # train_rnadata = train_rnadata.T
-    drug_data.index = range(drug_data.shape[0])
-    rna_data.index = range(rna_data.shape[0])
-
-    print('Preprocessing...!!!')
-    print(np.shape(rna_data), np.shape(drug_data))
-    # print(list(rna_data.columns))
-    return rna_data, drug_data
-
-
-def build_common_data(params: Dict):
-    """Construct common feature data frames.
-
-    :params: Dict params: A Python dictionary of CANDLE/IMPROVE keywords
-             and parsed values.
-    :params: Dict inputdtd: Path to directories of input data stored in
-            dictionary with str key str and Path value.
-
-    :return: drug and cell dataframes and smiles graphs
-    :rtype: pd.DataFrame
-    """    
-    # --------------------------------------------------------------------
-    # [Req] Create dataloaders
-    # --------------------------------------------------------------------
-    omics_loader = omics_utils.OmicsLoader(params)
-    drugs_loader = drugs_utils.DrugsLoader(params)
-
-    # ------------------------------------------------------
-    # [Req] Load X data (feature representations)
-    # ------------------------------------------------------
-
-    gene_expression = omics_loader.dfs['cancer_gene_expression.tsv']
-    df_drug = drugs_loader.dfs['drug_SMILES.tsv']
-    df_drug = df_drug.reset_index()
-    df_drug.columns = [params["drug_col_name"], "smiles"]
-    params['drug_id'] = params["drug_col_name"]
-    df_drug["SMILES"] = df_drug["smiles"]
-
-    # ------------------------------------------------------
-    # [MODEL] Preprocess X data
-    # ------------------------------------------------------
-    # Gene selection (based on LINCS landmark genes)
-    def gene_selection(df, genes_fpath, canc_col_name):
-        """ Takes a dataframe omics data (e.g., gene expression) and retains only
-        the genes specified in genes_fpath.
-        """
-        with open(genes_fpath) as f:
-            genes = [str(line.rstrip()) for line in f]
-        # genes = ["ge_" + str(g) for g in genes]  # This is for our legacy data
-        # print("Genes count: {}".format(len(set(genes).intersection(set(df.columns[1:])))))
-        genes = sorted(list(set(genes).intersection(set(df.columns[1:]))))
-        # genes = drp.common_elements(genes, df.columns[1:])
-        cols = [canc_col_name] + genes
-        return df[cols]
-
-    if params["use_lincs"]:
-        genes_fpath = filepath/"landmark_genes"
-        gene_expression = gene_selection(
-            gene_expression, genes_fpath, canc_col_name=params["canc_col_name"])
-
-    return df_drug, gene_expression
-
-
-
-
-def prepare_dataframe(args, gene_expression, smiles, responses):
-    gene_expression, drug_data = preprocess(args,
-                                            gene_expression, smiles, responses, args["y_col_name"])
-    drug_data = drug_data.drop(['index'], axis=1)
-    drug_columns = [
-        x for x in drug_data.columns if x not in [args["canc_col_name"], args["drug_col_name"]]]
-    # data = pd.merge(gene_expression, drug_data, on='DrugID', how='inner')
-    data = pd.merge(gene_expression, drug_data,
-                    on=args["canc_col_name"], how='inner')
-    gene_expression = gene_expression.drop([args["canc_col_name"]], axis=1)
-    gene_expression_columns = gene_expression.columns
-
-    return data, gene_expression_columns, drug_columns
-
+def gene_selection(df, genes_fpath, canc_col_name):
+    """ Takes a dataframe omics data (e.g., gene expression) and retains only
+    the genes specified in genes_fpath.
+    """
+    with open(genes_fpath) as f:
+        genes = [str(line.rstrip()) for line in f]
+    # genes = ["ge_" + str(g) for g in genes]  # This is for our legacy data
+    # print("Genes count: {}".format(len(set(genes).intersection(set(df.columns[1:])))))
+    genes = sorted(list(set(genes).intersection(set(df.columns[1:]))))
+    # genes = drp.common_elements(genes, df.columns[1:])
+    cols = [canc_col_name] + genes
+    return df[cols]
 
 def get_common_samples(
         df1: pd.DataFrame,
         df2: pd.DataFrame,
         ref_col: str):
-    """
-    Search for common data in reference column and retain only .
-
-    Args:
-        df1, df2 (pd.DataFrame): dataframes
-        ref_col (str): the ref column to find the common values
-
-    Returns:
-        df1, df2 after filtering for common data.
-
-    Example:
-        Before:
-
-        df1:
-        col1    ref_col     col3            col4
-        CCLE    ACH-000956      Drug_749        0.7153
-        CCLE    ACH-000325      Drug_1326       0.9579
-        CCLE    ACH-000475      Drug_490        0.213
-
-        df2:
-        ref_col     col2                col3                col4
-        ACH-000956  3.5619370596224327  0.0976107966264223      4.888499735514123
-        ACH-000179  5.202025844609336   3.5046203924035524      3.5058909297299574
-        ACH-000325  6.016139702655253   0.6040713236688608      0.0285691521967709
-
-        After:
-
-        df1:
-        col1    ref_col     col3            col4
-        CCLE    ACH-000956      Drug_749        0.7153
-        CCLE    ACH-000325      Drug_1326       0.9579
-
-        df2:
-        ref_col     col2                col3                col4
-        ACH-000956  3.5619370596224327  0.0976107966264223      4.888499735514123
-        ACH-000325  6.016139702655253   0.6040713236688608      0.0285691521967709
-    """
     # Retain df1 and df2 samples with common ref_col
     common_ids = list(set(df1[ref_col]).intersection(df2[ref_col]))
     df1 = df1[df1[ref_col].isin(common_ids)].reset_index(drop=True)
@@ -208,29 +71,6 @@ def get_common_samples(
 
 
 def scale_df(dataf, scaler_name="std", scaler=None, verbose=False):
-    """ Returns a dataframe with scaled data.
-
-    It can create a new scaler or use the scaler passed or return the
-    data as it is. If `scaler_name` is None, no scaling is applied. If
-    `scaler` is None, a new scaler is constructed. If `scaler` is not
-    None, and `scaler_name` is not None, the scaler passed is used for
-    scaling the data frame.
-
-    Args:
-        dataf: Pandas dataframe to scale.
-        scaler_name: Name of scikit learn scaler to apply. Options:
-                     ["minabs", "minmax", "std", "none"]. Default: std
-                     standard scaling.
-        scaler: Scikit object to use, in case it was created already.
-                Default: None, create scikit scaling object of
-                specified type.
-        verbose: Flag specifying if verbose message printing is desired.
-                 Default: False, no verbose print.
-
-    Returns:
-        pd.Dataframe: dataframe that contains drug response values.
-        scaler: Scikit object used for scaling.
-    """
     if scaler_name is None or scaler_name == "none":
         if verbose:
             print("Scaler is None (no df scaling).")
@@ -268,17 +108,37 @@ def scale_df(dataf, scaler_name="std", scaler=None, verbose=False):
 
 
 
+
+
+
+
+
+
+
+
 def run(params:Dict):
-    """ Run data preprocessing.
 
-    Args:
-        params (dict): dict of IMPROVE parameters and parsed values.
+    # --------------------------------------------------------------------
+    # [Req] Create dataloaders
+    # --------------------------------------------------------------------
+    omics_loader = omics_utils.OmicsLoader(params)
+    drugs_loader = drugs_utils.DrugsLoader(params)
 
-    Returns:
-        str: directory name that was used to save the ML data files.
-    """
+    # ------------------------------------------------------
+    # [Req] Load X data (feature representations)
+    # ------------------------------------------------------
 
-    df_drug_all, df_cell_all = build_common_data(params)
+    df_cell_all = omics_loader.dfs['cancer_gene_expression.tsv']
+    df_drug_all = drugs_loader.dfs['drug_SMILES.tsv']
+    df_drug_all = df_drug_all.reset_index()
+    df_drug_all.columns = [params["drug_col_name"], "smiles"]
+    params['drug_id'] = params["drug_col_name"]
+    df_drug_all["SMILES"] = df_drug_all["smiles"]
+
+    if params["use_lincs"]:
+        genes_fpath = filepath/"landmark_genes"
+        df_cell_all = gene_selection(df_cell_all, genes_fpath, canc_col_name=params["canc_col_name"])
+
     stages = {"train": params["train_split_file"],
             "val": params["val_split_file"],
             "test": params["test_split_file"]}
@@ -321,11 +181,31 @@ def run(params:Dict):
 
         # Sub-select desired response column (y_col_name)
         # And reduce response dataframe to 3 columns: drug_id, cell_id and selected drug_response
-        df_y = df_y[[params["drug_col_name"],
-                    params["canc_col_name"], params["y_col_name"]]]
+        df_y = df_y[[params["drug_col_name"], params["canc_col_name"], params["y_col_name"]]]
         # Combine data
-        data, gene_expression_columns, drug_columns = prepare_dataframe(params,
-                                                                        df_cell, df_drug_all, df_y)
+
+        obj = DataEncoding(params, params["input_supp_data_dir"], params["canc_col_name"],
+                            params["sample_col_name"], params["y_col_name"], params["drug_col_name"])
+
+        df_drug_stage = df_drug_all[[params['canc_col_name'], 'SMILES']]
+
+        smile_encode = pd.Series(df_drug_all['SMILES'].unique()).apply(obj._drug2emb_encoder)
+        uniq_smile_dict = dict(zip(df_drug_all['SMILES'].unique(), smile_encode))
+
+        df_drug_stage['drug_encoding'] = [uniq_smile_dict[i] for i in df_drug_stage['smiles']]
+        df_drug_stage = df_drug_stage.reset_index()
+
+        #response_data.columns = [params["canc_col_name"], params["drug_col_name"], 'Label']
+        df_drug_stage = pd.merge(df_y, df_drug_stage, on=params["drug_col_name"], how='inner')
+
+        df_drug_stage.index = range(df_drug_stage.shape[0])
+        df_cell.index = range(df_cell.shape[0])
+
+        drug_data = drug_data.drop(['index'], axis=1)
+        drug_columns = [x for x in drug_data.columns if x not in [params["canc_col_name"], params["drug_col_name"]]]
+        data = pd.merge(df_cell, drug_data, on=params["canc_col_name"], how='inner')
+        df_cell = df_cell.drop([params["canc_col_name"]], axis=1)
+        gene_expression_columns = df_cell.columns
 
         # --------------------------------------------------------------------
         # [MODEL] Save X data
@@ -358,9 +238,6 @@ def main(args):
         filepath,
         default_config="deepttc_params.txt",
         additional_definitions=preprocess_params)
-
-    #download_model_data(params)
-    # download_dataset(params)
 
     ml_data_outdir = run(params)
     print("\nFinished data preprocessing.")
