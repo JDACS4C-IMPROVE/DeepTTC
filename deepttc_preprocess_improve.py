e228f34600f82f74c8248d9ca2cbfe7979520979
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-07-07 09:33:16 -0400

    remove nan responses

--- deepttc_preprocess_improve.py
+++ deepttc_preprocess_improve.py
@@ -47,6 +47,7 @@ def run(params:Dict):
     response_train = frm.get_y_data(split_file=params["train_split_file"], 
                                    benchmark_dir=params['input_dir'], 
                                    y_data_file=params['y_data_file'])
+    response_train = response_train.dropna(subset=[params['y_col_name']])
     print("Find intersection of training data.")
     response_train = frm.get_y_data_with_features(response_train, ge, params['canc_col_name'])
     response_train = frm.get_y_data_with_features(response_train, smiles, params['drug_col_name'])
@@ -70,6 +71,7 @@ def run(params:Dict):
         response_stage = frm.get_y_data(split_file=split_file, 
                                 benchmark_dir=params['input_dir'], 
                                 y_data_file=params['y_data_file'])
+        response_stage = response_stage.dropna(subset=[params['y_col_name']])
         response_stage = frm.get_y_data_with_features(response_stage, ge, params['canc_col_name'])
         response_stage = frm.get_y_data_with_features(response_stage, smiles, params['drug_col_name'])
         ge_stage = frm.get_features_in_y_data(ge, response_stage, params['canc_col_name'])
